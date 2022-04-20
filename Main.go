2f639f776ac71f2d6a7290cc10446c31a195eac2
dakotaihli <dakota.ihli@gmail.com>
2022-04-20 11:36:11 -0400

    Problem 2

--- Main.go
+++ Main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func problem1() {
 	upperLimit := 1000
 	var sum int
 	for i := 0; i < upperLimit; i++ {
@@ -11,4 +11,29 @@ func main() {
 		}
 	}
 	fmt.Println(sum)
+}
+
+func fibonacci(n int) int {
+	if n <= 0 {
+		return 1
+	} else if n == 1 {
+		return 2
+	} else {
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
+}
+
+func problem2() {
+	upperLimit := 4000000
+	var sum int
+	for i := 0; fibonacci(i) < upperLimit; i++ {
+		if i%3 == 1 {
+			sum += fibonacci(i)
+		}
+	}
+	fmt.Println(sum)
+}
+
+func main() {
+	problem2()
 }
