831e5c706dabc0960036c964db369e8e049e7a6b
dakotaihli <dakota.ihli@gmail.com>
2022-04-20 09:46:28 -0400

    Minimum working example: Problem 1

--- Main.go
+++ Main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	upperLimit := 1000
+	var sum int
+	for i := 0; i < upperLimit; i++ {
+		if (i%3 == 0) || (i%5 == 0) {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/projeuler
+
+go 1.18
